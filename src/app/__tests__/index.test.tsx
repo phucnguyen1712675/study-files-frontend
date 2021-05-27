7d96a2092cb985213ec3c16dc638a4c416dc6c47
NGUYỄN THÀNH VĨNH PHÚC <1712675@student.hcmus.edu.vn>
2021-05-27 16:23:23 +0700

    my first commit

--- src/app/__tests__/index.test.tsx
+++ src/app/__tests__/index.test.tsx
@@ -5,6 +5,18 @@ import { App } from '../index';
 
 const renderer = createRenderer();
 
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str: any) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+}));
+
 describe('<App />', () => {
   it('should render and match the snapshot', () => {
     renderer.render(<App />);
