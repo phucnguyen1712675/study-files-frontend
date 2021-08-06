42ae24f1032de9d14c6090810b1d7f28b7070409
phucnguyen1712675 <pbadboyv@gmail.com>
2021-08-06 23:22:09 +0700

    Update models

--- src/model/query/getting_teacher_courses/index.ts
+++
@@ -1,9 +0,0 @@
-type SortResults = 'asc' | 'desc';
-
-export type GettingTeacherCoursesQuery = {
-  teacherId: string;
-  page: number;
-  limit: number;
-  sortBy?: string;
-  sortResults?: SortResults;
-};
