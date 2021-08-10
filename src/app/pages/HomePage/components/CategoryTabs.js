25a2c38ff280777b0c9c6794a2b7a5d8e9d75e4c
Sammatha@99 <ngonhatrang99@gmail.com>
2021-08-10 23:17:27 +0700

    check if category have courses to show in homepage

--- src/app/pages/HomePage/components/CategoryTabs.js
+++ src/app/pages/HomePage/components/CategoryTabs.js
@@ -1,25 +1,51 @@
 import React, { useContext } from 'react';
 import SubCategoryTabs from './SubCategoryTabs';
 import AppContext from '../../../AppContext';
+import { LensTwoTone } from '@material-ui/icons';
 
 export default function CategoryTabs({ category }) {
   const { store } = useContext(AppContext);
-  return (
-    <>
-      <div style={{ margin: '20px' }}>
-        <h1 style={{ margin: '20px 20px 5px', color: '#387CFF' }}>
-          {category.name}
-        </h1>
-        {store.subCategories
-          .filter(subCategory => subCategory.categoryId === category.id)
-          .map(subCategory => (
-            <SubCategoryTabs
-              subCategory={subCategory}
-              isNavigate={true}
-              key={subCategory.id}
-            />
-          ))}
-      </div>
-    </>
-  );
+  const categoriesWidget = function () {
+    let isHaveCourse = false;
+    let subCategories = Array.from(
+      store.subCategories.filter(
+        subCategory => subCategory.categoryId === category.id,
+      ),
+    );
+    if (subCategories.length > 0) {
+      for (var subCategory of subCategories) {
+        const courses = Array.from(
+          store.latestCourses.filter(
+            course => course.subCategoryId === subCategory.id,
+          ),
+        );
+        if (courses.length > 0) {
+          isHaveCourse = true;
+          break;
+        }
+      }
+    }
+
+    if (isHaveCourse) {
+      return (
+        <div style={{ margin: '20px' }}>
+          <h1 style={{ margin: '20px 20px 5px', color: '#387CFF' }}>
+            {category.name}
+          </h1>
+          {store.subCategories
+            .filter(subCategory => subCategory.categoryId === category.id)
+            .map(subCategory => (
+              <SubCategoryTabs
+                subCategory={subCategory}
+                isNavigate={true}
+                key={subCategory.id}
+              />
+            ))}
+        </div>
+      );
+    } else {
+      return <></>;
+    }
+  };
+  return <>{categoriesWidget()}</>;
 }
